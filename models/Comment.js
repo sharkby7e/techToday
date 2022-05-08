6a76b54ed5a08551d29d795242555397acb32c8d
sharkbyte <sgquin@gmail.com>
2022-05-07 23:43:38 -0700

    defined relationships between models

--- models/Comment.js
+++ models/Comment.js
@@ -16,6 +16,13 @@ Comment.init(
       allowNull: false,
       defaultValue: DataTypes.NOW,
     },
+    post_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "post",
+        key: "id",
+      },
+    },
     user_id: {
       type: DataTypes.INTEGER,
       references: {
--- server.js
+++ server.js
@@ -10,19 +10,18 @@ const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+app.use(routes);
 
-// Set up Handlebars.js engine with custom helpers
-// Inform Express.js on which template engine to use
 // const hbs = exphbs.create({helpers});
 // app.engine("handlebars", hbs.engine);
 // app.set("view engine", "handlebars");
+
 const hbs = exphbs.create({
   // helpers: helpers,
   extname: ".hbs",
 });
 app.engine("hbs", hbs.engine);
 app.set("view engine", "hbs");
-// app.set("views", "./views");
 
 const sess = {
   secret: "SuperDuper Secreter Secret Secret",
@@ -40,8 +39,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use(routes);
-
-sequelize.sync({ force: false }).then(() => {
+sequelize.sync({ force: true }).then(() => {
   app.listen(PORT, () => console.log("Now listening"));
 });
